3210c8848dd71bd53a9b3fd3f9f3f6460eda1812
gabriellaguerre <laguerre.g.g@gmail.com>
2022-12-15 13:38:52 -0500

    first commit

---
+++ math.js
@@ -0,0 +1,13 @@
+window.addEventListener("DOMContentLoaded", () => {
+    const div1 = document.createElement("div")
+    div1.setAttribute("id", "number1");
+    document.body.append(div1)
+
+    const div2 = document.createElement("div");
+    div2.setAttribute("id", "number2");
+    document.body.append(div2)
+
+    const div3 = document.createElement("div");
+    div3.setAttribute("id", "number3");
+    document.body.append(div3)
+})
